2b29958570226f756248306b5c16656921ce2d28
Raghavendra Tikare <tikare@hyrulecastle.molgen.mpg.de>
2021-08-23 16:44:19 +0200

    h

--- mstBackbone.h
+++ mstBackbone.h
@@ -191,8 +191,8 @@ void MSTBackbone::MSTBackboneOverlappingSets() {
 		if (computeLocalPhylogeneticTree) {
 			this->M->SetIdsOfExternalVertices();
 			idsOfExternalVertices = this->M->idsOfExternalVertices;
-			idsOfVerticesForSEM = this->M->vertexMap_ptr->idsOfVerticesInSubtree;
-			this->numberOfVerticesInSubtree = this->M->vertexMap_ptr->idsOfVerticesInSubtree.size();
+			idsOfVerticesForSEM = this->M->vertexMap->idsOfVerticesInSubtree;
+			this->numberOfVerticesInSubtree = this->M->vertexMap->idsOfVerticesInSubtree.size();
 			for (int id: idsOfExternalVertices) {
 				idsOfVerticesForSEM.push_back(id);
 			}
