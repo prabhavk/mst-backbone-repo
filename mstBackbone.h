9b1d845a0703ad6b5ea60d303bf446522c5118f1
Raghavendra Tikare <tikare@hyrulecastle.molgen.mpg.de>
2021-08-24 13:01:09 +0200

    g

--- mstBackbone.h
+++ mstBackbone.h
@@ -171,7 +171,7 @@ void MSTBackbone::MSTBackboneOverlappingSets() {
 	idsOfVerticesForSEM.clear();
 	for (pair <int, MST_vertex *>  vertexIDpair : * this->M->vertexMap) {
 		v = vertexIDpair.second;
-		idsOfVerticesForSEM.push_back(v);
+		idsOfVerticesForSEM.push_back(vertexIDpair.second);
 	}
 	tie (names, sequences, sitePatternWeights, sitePatternRepetitions) = this->M->GetCompressedSequencesSiteWeightsAndSiteRepeats(idsOfVerticesForSEM);	
 	this->T->sequenceFileName = this->sequenceFileName;
