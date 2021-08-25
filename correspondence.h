34bcaceb6cf082ee2ec89449846e86ba74489205
prabhavk <prabhavk@mpi-inf.mpg.de>
2021-08-25 10:51:44 +0200

    added header file for correspondence

---
+++ correspondence.h
@@ -0,0 +1,18 @@
+#ifndef CORRESPONDENCE_H
+#define CORRESPONDENCE_H
+
+
+//////////////////////////////////////////////////
+/// Splits in unrooted generally labeled trees ///
+//////////////////////////////////////////////////
+
+
+// unrooted tree class
+// Read tree in edge list format (format in which MST is stored)
+// Read tree in neiwck format (commonly used for phylogenies)
+// Compute splits for generally labeled tree
+// Computing correspondence
+// Report/display correspondence
+
+
+#endif
