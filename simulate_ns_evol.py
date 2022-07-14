215d44b9e862c3e1d4cba677e263edd688945b71
prabhavk <prabhavk@mpi-inf.mpg.de>
2022-07-14 17:56:56 +0200

    updated script for ns evol

--- simulate_ns_evol.py
+++ simulate_ns_evol.py
@@ -1,8 +1,12 @@
 # Conditions under which systematic bias appears in Grove
 import ete3
 from fileIO import ReadRootedTree, ReadTree
+from MarkovModels import GenerateQForStationaryDistribution, Get11FreeRates
+from config import projectPath
+
+
 tree_id = "0"
-tree_file_name = "/home/kalaghat/exaptation/Projects/MSTBasedForests/data/RAxMLGrove/trees/" + tree_id + "/tree_best.newick"
+tree_file_name = projectPath + "MSTBasedForests/data/RAxMLGrove/trees/" + tree_id + "/tree_best.newick"
 # T = ete3.Tree(tree_file_name)
 T = ReadRootedTree(tree_file_name,"newick")
 # T.ladderize(direction=0)
@@ -27,6 +31,39 @@ for node in preOrderTraversal:
 
 branch_length_ratio.sort(key=lambda x: x[1])
 branch_length_ratio[0]
+
+at = 0.3
+# gc = 1 - at
+pi_at = [at/2, (1-at)/2, at/2, (1-at)/2]
+print(pi_at)
+pi_gc = [(1-at)/2, at/2, (1-at)/2, at/2]
+print(pi_gc)
+
+Q_at = GenerateQForStationaryDistribution(pi_at)
+Q_gc = GenerateQForStationaryDistribution(pi_gc)
+
+rateVector_at = Get11FreeRates(Q_at)
+rateVector_gc = Get11FreeRates(Q_gc)
+T.rateVectorForCat[1] = rateVector_at
+T.rateVectorForCat[2] = rateVector_gc
+
+for vertex in T.vertices:
+    T.rateCategoryForVertex[vertex] = 1
+
+T.rateCategoryForVertex['tax2'] = 2
+T.rateCategoryForVertex['tax3'] = 2
+
+sequenceLength = 10000 # 
+# p_change = 0.5
+# RT.GenerateMarkovModulatedMarkovModel(p_change)
+sequence_file_suffix = 'sequences_tree_id_42_num_taxa_4_sequence_length_' + str(sequenceLength)
+sequence_file_name_sim = projectPath + 'MSTBasedForests/data/quartet_ns/' + sequence_file_suffix + '.fas'
+T.sequence_file_suffix = sequence_file_suffix
+
+
+T.control_file_name = projectPath + 'MSTBasedForests/data/quartet_ns/control.txt'
+T.WriteControlFile()
+T.SimulateEvolutionUsingIndelible()
 
 
 
